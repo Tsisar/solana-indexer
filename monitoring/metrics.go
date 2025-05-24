4a2919b55b13b24c917771bac07c74872cf2d7c9
Tsisar <fhantom.tp@gmail.com>
2025-05-24 13:39:16 +0400

    feat(grafana): add initial dashboard for slot metrics (fetcher, parser, listener)

--- monitoring/metrics.go
+++ monitoring/metrics.go
@@ -12,17 +12,17 @@ var (
 		},
 	)
 
-	ParserCurrentSlot = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: "indexer_parser_current_slot",
-			Help: "Current slot being parsed by the parser/indexer",
-		},
-	)
-
 	ListenerCurrentSlot = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "indexer_listener_current_slot",
 			Help: "Current slot received by the WebSocket listener",
+		},
+	)
+
+	ParserCurrentSlot = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "indexer_parser_current_slot",
+			Help: "Current slot being parsed by the parser/indexer",
 		},
 	)
 )
