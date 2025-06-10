79ec26020f736f55ee95a3fa131e904a24ed821f
Tsisar <fhantom.tp@gmail.com>
2025-06-10 15:33:04 +0400

    Update ci-cd

--- internal/storage/model/generic/generic.go
+++ internal/storage/model/generic/generic.go
@@ -54,7 +54,6 @@ func LoadWithPreloads[T Identifiable](
 	}
 }
 
-// func Save[T any](ctx context.Context, db *gorm.DB, model T) error {
 func Save[T Identifiable](ctx context.Context, db *gorm.DB, model T) error {
 	return db.WithContext(ctx).
 		Clauses(clause.OnConflict{
